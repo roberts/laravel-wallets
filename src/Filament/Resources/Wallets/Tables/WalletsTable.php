8d965daf96918588707139c6946695751c3fe05a
drewroberts <24581081+drewroberts@users.noreply.github.com>
2025-09-20 16:34:39 +0000

    Fix styling

--- src/Filament/Resources/Wallets/Tables/WalletsTable.php
+++ src/Filament/Resources/Wallets/Tables/WalletsTable.php
@@ -2,10 +2,6 @@
 
 namespace Roberts\LaravelWallets\Filament\Resources\Wallets\Tables;
 
-use Filament\Actions\BulkActionGroup;
-use Filament\Actions\DeleteAction;
-use Filament\Actions\DeleteBulkAction;
-use Filament\Actions\EditAction;
 use Filament\Actions\ViewAction;
 use Filament\Tables\Columns\TextColumn;
 use Filament\Tables\Filters\SelectFilter;
